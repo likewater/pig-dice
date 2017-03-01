f0b49950b49b6349b6b76dd95cc03814f5d7217b
Blake Womack and Kaya Winn <blake@>
2017-03-01 11:11:02 -0800

    adds labels to panels

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,45 @@
-//vARIABLES
+// VARIABLES
+var turnScore = 0;
+var player1;
+var player2;
+var goal = 100;
+var currentPlayer;
+var currentScore;
+var rollValue;
+
+// PLAYER CONSTRUCTOR - CREATES PLAYER OBJECTS
+function Player(name){
+  this.name = name;
+  this.totalScore = 0;
+};
+
+// FUNCTION SCORE - CREATES SCORE OBJECT
+function currentScore(playerName, rollValue){
+  this.player
+  this.currentScore;
+};
+
+// FUNCTION ROLL - SIMULATES ROLL OF ONE DICE AND RETURNS VALUES 1 THROUGH 6
+function Roll(){
+//math random function
+return number to Score function  
+};
+
+// ON CLICK OF ROLL BUTTON PERFORMS THE ROLL FUNCTION
+  $("#roll").click(function() {
+
+  });
+
+// ON CLICK OF HOLD ADDS CURRENT ROLL SCORE TO PLAYERS CURRENTSCORE IN PLAYER OBJECT AND SWITCHES TO OTHER PLAYER
+  $("#hold").click(function(){
+    // MAKE SCREEN CHANGE TO SHOW OTHER PLAYER
+  });
+
+// PAGE LOAD FUNCTION
+ $(document).ready(function(){
+   $("form#new-contact").submit(function(event) {
+ event.preventDefault();
 
 
-//PLAYER CONSTRUCTOR
-
-//FUNCTION ROLL
-
-
-//PAGE LOAD FUNCTION
+    //SET PLAYER OBJECT NAME WITH NAME FROM FORM VALUE
+});
